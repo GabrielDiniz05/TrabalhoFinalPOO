b0289ce8e68f0372525b4a7be4f0c8dec07f3821
Gabriel Diniz <gabriel.assisdiniz@gmail.com>
2023-11-23 11:53:21 -0300

    Teste

--- sistema/ContaAgua.cs
+++ sistema/ContaAgua.cs
@@ -1,28 +1,43 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace TrabalhoFinalPOO.sistema
-{
-    public class ContaAgua : Conta
-    {
-        public void LerMesAtual()
-        {
-            Console.WriteLine("");
+namespace TrabalhoFinalPOO.sistema{
+    public class ContaAgua : Conta{
 
+        private double tarifaAgua = 0;
+        private double tarifaEsgoto = 0;
+
+        private double imposto = 1.03;
+
+        public ContaAgua(string tipoImovel, double leituraMesAnterior, double leituraMesAtual) : base(){
+            this.tipoImovel = tipoImovel;
+            this.leituraMesAnterior = leituraMesAnterior;
+            this.leituraMesAtual = leituraMesAtual;
         }
-        public void LerMesAnterior()
-        {
-            Console.WriteLine("");
-        }
-        public double Consumo()
-        {
-            return 1;
-        }
-        public double ValorTotal()
-        {
-            return 1;
+
+        public override double ValorTotalComImposto(){
+            switch(tipoImovel){
+                case "RESIDENCIAL":
+                    double aux = consumo;
+
+                    if(aux < 6){
+                        tarifaAgua = 10.08;
+                        tarifaEsgoto = 5.05;
+                        valorSemImposto = tarifaAgua + tarifaEsgoto;
+                        return valorSemImposto*=imposto;
+                    }else if(aux >= 6 && aux < 10){
+                        tarifaAgua = 2.241;
+                        tarifaEsgoto = 1.122;
+                    }
+
+                    break;
+
+                case "COMERCIAL":
+                    
+                    break;
+                
+                default:
+                    throw new Exception("Teste");
+            }
         }
     }
 }
--- sistema/ContaLuz.cs
+++ sistema/ContaLuz.cs
@@ -1,23 +1,40 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace TrabalhoFinalPOO.sistema
 {
     public class ContaLuz : Conta
     {
-        public void LerMesAtual(){
-                            
-        }
-        public void LerMesAnterior(){
+        private double valorFixoSerPago = 13.25;
 
+        public ContaLuz(string tipoImovel, double leituraMesAnterior, double leituraMesAtual) : base(){
+            this.tipoImovel = tipoImovel;
+            this.leituraMesAnterior = leituraMesAnterior;
+            this.leituraMesAtual = leituraMesAtual;
         }
-        public double Consumo(){
-            return 1;
-        }
-        public double ValorTotal(){
-            return 1;
+
+        public override ValorTotalComImposto(){
+            switch(tipoImovel){
+                case "COMERCIAL":
+                    double tarifa = 0.41;
+                    double total = (consumo * tarifaComercial) + valorFixoSerPago;
+                    valorSemImposto = total;
+                    if(consumo > 90){
+                        total*=1.2195;
+                    }
+                    break;
+
+                case "RESIDENCIAL":
+                    double tarifa = 0.46;
+                    double total = (consumo * tarifaComercial) + valorFixoSerPago;
+                    valorSemImposto = total;
+                    if(consumo > 90){
+                        total*=1.4285;
+                    }
+                    break;
+                
+                default:
+                    throw new Exception("Teste");
+            }
         }
     }
 }
