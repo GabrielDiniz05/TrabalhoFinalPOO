11da401b64fde9d446ceb8649605b250a602b2fa
Gabriel Diniz <gabriel.assisdiniz@gmail.com>
2023-11-23 12:12:39 -0300

    ContaDeluz

--- sistema/ContaLuz.cs
+++ sistema/ContaLuz.cs
@@ -4,17 +4,42 @@ namespace TrabalhoFinalPOO.sistema
 {
     public class ContaLuz : Conta
     {
-        public void LerMesAtual(){
-                            
-        }
-        public void LerMesAnterior(){
+        private double valorFixoSerPago = 13.25;
 
+        public ContaLuz(string tipoImovel, double leituraMesAnterior, double leituraMesAtual) : base()
+        {
+            this.tipoImovel = tipoImovel;
+            this.leituraMesAnterior = leituraMesAnterior;
+            this.leituraMesAtual = leituraMesAtual;
         }
-        public double Consumo(){
-            return 1;
-        }
-        public double ValorTotal(){
-            return 1;
+
+        public override double ValorTotalComImposto()
+        {
+            switch (tipoImovel)
+            {
+                case "COMERCIAL":
+                    double tarifa = 0.41;
+                    double total = (consumo * tarifa) + valorFixoSerPago;
+                    valorSemImposto = total;
+                    if (consumo > 90)
+                    {
+                        total *= 1.2195;
+                    }
+                    return total;
+
+                case "RESIDENCIAL":
+                    tarifa = 0.46;
+                    total = (consumo * tarifa) + valorFixoSerPago;
+                    valorSemImposto = total;
+                    if (consumo > 90)
+                    {
+                        total *= 1.4285;
+                    }
+                    return total;
+
+                default:
+                    throw new Exception("Teste");
+            }
         }
     }
 }
