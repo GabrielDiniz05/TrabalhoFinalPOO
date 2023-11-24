7987e4b6b1900ced638ed31892b08565b6382b46
Abdon França neto <oliabdon006@gmail.com>
2023-11-23 23:37:12 -0300

    Commit paia

--- Program.cs
+++ Program.cs
@@ -7,29 +7,53 @@ public class Program
 
     public static void Main(String[] args)
     {
-        Consumidor c1;
-        Console.WriteLine("SEJA BEM VINDO AO NOSSO SISTEMA!");
-        Console.WriteLine("PARA OS SEGUINTES PROCESSOS, DIGITE UM NUMERO PARA REALIZA-LO.");
-        Console.WriteLine("CADASTRAR-SE: 1");
-        Console.WriteLine("LOGAR: 2");
-        Console.WriteLine("SAIR DO SISTEMA: 3");
-
-        string senha = Console.ReadLine();
-
-        switch (senha)
+        bool isRunning = true;
+        while (isRunning)
         {
-            case "1":
-                Console.WriteLine("Digite seu nome: ");
-                string nome = Console.ReadLine();
-                c1 = new Consumidor(nome);
-                break;
-            case "2":
-                Console.WriteLine("Digite seu nome: ");
-                nome = Console.ReadLine();
-                c1 = new Consumidor(nome);
-                break;
-            case "3":
-            break;
+            Console.WriteLine("SEJA BEM VINDO AO NOSSO SISTEMA!");
+            Console.WriteLine("PARA OS SEGUINTES PROCESSOS, DIGITE UM NUMERO PARA REALIZA-LO.");
+            Console.WriteLine("CADASTRAR-SE: 1");
+            Console.WriteLine("LOGAR: 2");
+            Console.WriteLine("SAIR DO SISTEMA: 3");
+            string Opcao = Console.ReadLine();
+            if(Opcao == "3"){
+                isRunning = false;
+            }
+            MenuInicial(Opcao);
+            
+        }
+
+
+
+        static void MenuInicial(string Opcao)
+        {
+            Consumidor c1;
+            string senha;
+            string nome;
+            switch (Opcao)
+            {
+                case "1":
+                    Console.WriteLine("Digite seu nome: ");
+                    nome = Console.ReadLine();
+                    Console.WriteLine("Digite sua senha: ");
+                    senha = Console.ReadLine();
+                    c1 = new Consumidor(nome, senha);
+                    c1.enviarParaBanco();
+                    break;
+                case "2":
+                    Console.WriteLine("Digite seu nome: ");
+                    nome = Console.ReadLine();
+                    Console.WriteLine("Digite sua senha: ");
+                    senha = Console.ReadLine();
+                    c1 = new Consumidor(nome, senha);
+                    break;
+            }
+        }
+
+
+        static void MenuContas()
+        {
+            
         }
     }
 }
