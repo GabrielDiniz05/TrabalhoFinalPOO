a43e3429698f49646cce238812c3e6536bb3f63e
Gabriel Diniz <gabriel.assisdiniz@gmail.com>
2023-11-24 22:43:06 -0300

    ValortotalTroca

--- sistema/ContaAgua.cs
+++ sistema/ContaAgua.cs
@@ -1,9 +1,7 @@
 using System;
 
-namespace TrabalhoFinalPOO.sistema
-{
-    public class ContaAgua : Conta
-    {
+namespace TrabalhoFinalPOO.sistema{
+    public class ContaAgua : Conta{
 
         private double tarifaAgua = 0;
         private double tarifaEsgoto = 0;
@@ -12,55 +10,42 @@ namespace TrabalhoFinalPOO.sistema
 
         private double ValorTotal = 0;
 
-        public ContaAgua(string tipoImovel, double leituraMesAnterior, double leituraMesAtual) : base()
-        {
+        public ContaAgua(string tipoImovel, double leituraMesAnterior, double leituraMesAtual) : base(){
             this.tipoImovel = tipoImovel;
             this.leituraMesAnterior = leituraMesAnterior;
             this.leituraMesAtual = leituraMesAtual;
             consumo = this.leituraMesAtual - this.leituraMesAnterior;
         }
 
-        public override double ValorTotalComImposto()
-        {
-            switch (tipoImovel)
-            {
+        public override double ValorTotalComImposto(){
+            switch (tipoImovel){
                 case "RESIDENCIAL":
                     double aux = consumo;
                     double consumoFinalAgua = 0;
                     double consumoFinalEsgoto = 0;
                     double count = 0;
-                    if (aux >= 6)
-                    {
-
-                        if (aux >= 10)
-                        {
+                    if (aux >= 6){
+                        if (aux >= 10){
                             tarifaAgua = 2.241;
                             tarifaEsgoto = 1.122;
                             consumoFinalAgua += tarifaAgua * 10;
                             consumoFinalEsgoto += tarifaEsgoto * 10;
                             count += 10;
-                        }
-                        else
-                        {
+                        }else{
                             tarifaAgua = 2.241;
                             tarifaEsgoto = 1.122;
                             consumoFinalAgua += aux * tarifaAgua;
                             consumoFinalEsgoto += aux * tarifaEsgoto;
                             count = aux;
                         }
-                        if (aux - count > 0)
-                        {
-
-                            if (aux - count >= 10)
-                            {
+                        if (aux - count > 0){
+                            if (aux - count >= 10){
                                 tarifaAgua = 5.447;
                                 tarifaEsgoto = 2.724;
                                 consumoFinalAgua += tarifaAgua * 5;
                                 consumoFinalEsgoto += tarifaEsgoto * 5;
                                 count += 5;
-                            }
-                            else
-                            {
+                            }else{
                                 tarifaAgua = 5.447;
                                 tarifaEsgoto = 2.724;
                                 consumoFinalAgua += (aux - count) * tarifaAgua;
@@ -68,18 +53,14 @@ namespace TrabalhoFinalPOO.sistema
                                 count = aux;
                             }
                         }
-                        if (aux - count > 0)
-                        {
-                            if (aux - count >= 15)
-                            {
+                        if (aux - count > 0){
+                            if (aux - count >= 15){
                                 tarifaAgua = 5.461;
                                 tarifaEsgoto = 2.731;
                                 consumoFinalAgua += tarifaAgua * 5;
                                 consumoFinalEsgoto += tarifaEsgoto * 5;
                                 count += 5;
-                            }
-                            else
-                            {
+                            }else{
                                 tarifaAgua = 5.461;
                                 tarifaEsgoto = 2.731;
                                 consumoFinalAgua += (aux - count) * tarifaAgua;
@@ -87,18 +68,14 @@ namespace TrabalhoFinalPOO.sistema
                                 count = aux;
                             }
                         }
-                        if (aux - count > 0)
-                        {
-                            if (aux - count >= 20)
-                            {
+                        if (aux - count > 0){
+                            if (aux - count >= 20){
                                 tarifaAgua = 5.487;
                                 tarifaEsgoto = 2.744;
                                 consumoFinalAgua = +tarifaAgua * 20;
                                 consumoFinalEsgoto = +tarifaEsgoto * 20;
                                 count += 20;
-                            }
-                            else
-                            {
+                            }else{
                                 tarifaAgua = 5.487;
                                 tarifaEsgoto = 2.744;
                                 consumoFinalAgua += (aux - count) * tarifaAgua;
@@ -106,60 +83,47 @@ namespace TrabalhoFinalPOO.sistema
                                 count = aux;
                             }
                         }
-
-                        if (aux - count > 0)
-                        {
+                        if (aux - count > 0){
                             tarifaAgua = 10.066;
                             tarifaEsgoto = 5.035;
                             consumoFinalAgua += (aux - count) * tarifaAgua;
                             consumoFinalEsgoto += (aux - count) * tarifaEsgoto;
                         }
-
-                    }
-                    else
-                    {
+                    }else{
                         tarifaAgua = 10.08;
                         tarifaEsgoto = 5.05;
                     }
                     valorSemImposto = tarifaAgua + tarifaEsgoto;
-                    return valorSemImposto *= imposto; // Talvez Trocar
+                    ValorTotal = valorSemImposto *= imposto;
+                    return ValorTotal; // Talvez Trocar
 
                 case "COMERCIAL":
                     aux = consumo;
                     consumoFinalAgua = 0;
                     consumoFinalEsgoto = 0;
                     count = 0;
-                    if (aux >= 6)
-                    {
-                        if (aux >= 10)
-                        {
+                    if (aux >= 6){
+                        if (aux >= 10){
                             tarifaAgua = 4.299;
                             tarifaEsgoto = 2.149;
                             consumoFinalAgua += tarifaAgua * 10;
                             consumoFinalEsgoto += tarifaEsgoto * 10;
                             count += 10;
-                        }
-                        else
-                        {
+                        }else{
                             tarifaAgua = 4.299;
                             tarifaEsgoto = 2.149;
                             consumoFinalAgua += aux * tarifaAgua;
                             consumoFinalEsgoto += aux * tarifaEsgoto;
                             count = aux;
                         }
-                        if (aux - count > 0)
-                        {
-
-                            if (aux - count >= 10)
-                            {
+                        if (aux - count > 0){
+                            if (aux - count >= 10){
                                 tarifaAgua = 8.221;
                                 tarifaEsgoto = 4.111;
                                 consumoFinalAgua += tarifaAgua * 30;
                                 consumoFinalEsgoto += tarifaEsgoto * 30;
                                 count += 30;
-                            }
-                            else
-                            {
+                            }else{
                                 tarifaAgua = 8.221;
                                 tarifaEsgoto = 4.111;
                                 consumoFinalAgua += (aux - count) * tarifaAgua;
@@ -167,18 +131,14 @@ namespace TrabalhoFinalPOO.sistema
                                 count = aux;
                             }
                         }
-                        if (aux - count > 0)
-                        {
-                            if (aux - count >= 40)
-                            {
+                        if (aux - count > 0){
+                            if (aux - count >= 40){
                                 tarifaAgua = 8.288;
                                 tarifaEsgoto = 4.144;
                                 consumoFinalAgua += tarifaAgua * 60;
                                 consumoFinalEsgoto += tarifaEsgoto * 60;
                                 count += 60;
-                            }
-                            else
-                            {
+                            }else{
                                 tarifaAgua = 8.288;
                                 tarifaEsgoto = 4.144;
                                 consumoFinalAgua += (aux - count) * tarifaAgua;
@@ -186,23 +146,19 @@ namespace TrabalhoFinalPOO.sistema
                                 count = aux;
                             }
                         }
-
-                        if (aux - count > 0)
-                        {
+                        if (aux - count > 0){
                             tarifaAgua = 8.329;
                             tarifaEsgoto = 4.165;
                             consumoFinalAgua += (aux - count) * tarifaAgua;
                             consumoFinalEsgoto += (aux - count) * tarifaEsgoto;
                         }
-                    }
-                    else
-                    {
+                    }else{
                         tarifaAgua = 25.79;
                         tarifaEsgoto = 12.90;
                     }
                     valorSemImposto = tarifaAgua + tarifaEsgoto;
-                    return valorSemImposto *= imposto; // Talvez Trocar
-
+                    ValorTotal = valorSemImposto *= imposto;
+                    return ValorTotal; // Talvez Trocar
 
 
                 default:
